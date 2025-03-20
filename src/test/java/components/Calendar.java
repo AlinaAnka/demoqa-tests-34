2d6b7b1f30d122bcb4af0eea29084848b2840f32
Alina K <alina885@mail.ru>
2025-03-20 16:43:54 +0300

    added tests

---
+++ src/test/java/components/Calendar.java
@@ -0,0 +1,15 @@
+package components;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class Calendar {
+
+    public void setDate(String day, String month, String year){
+
+        $("select.react-datepicker__year-select").selectOption("1905");
+        $("select.react-datepicker__month-select").selectOption("April");
+        $("div.react-datepicker__day--005").click();
+
+
+    }
+}
---
+++ src/test/java/tests/PageObjectsTests.java
@@ -0,0 +1,46 @@
+package tests;
+
+import components.RegistrationPage;
+import org.junit.jupiter.api.Test;
+
+public class PageObjectsTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void objectsTest() {
+        registrationPage.openPage()
+                .setFirstName("Alina")
+                .setLastName("Kovrigina")
+                .setEmail("alina885@mail.ru")
+                .setGender("Female")
+                .setUserNumber("1122334455")
+                .setDateOfBirth("5", "April", "1905")
+                .setSubjects("Math")
+                .setHobbies("Sports")
+                .setUploadPicture("picture.png")
+                .setCurrentAddress("Moscow55")
+                .setState("Haryana")
+                .setCity("Karnal")
+                .setSubmit();
+
+
+        registrationPage.checkResult("Student Name", "Alina Kovrigina")
+                .checkResult("Student Email", "lina885@mail.ru")
+                .checkResult("Gender", "Female")
+                .checkResult("Mobile", "1122334455")
+                .checkResult("Date of Birth", "05 April,1905")
+                .checkResult("Subjects", "Math")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "picture.png")
+                .checkResult("Address", "Moscow55")
+                .checkResult("State and City", "Haryana Karnal");
+
+
+
+
+    }
+    }
+
+
+
