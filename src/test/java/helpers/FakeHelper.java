9884ed3fed99a0fe03616abd53059f035c232988
Alina K <alina885@mail.ru>
2025-03-25 11:29:28 +0300

    added tests

--- src/test/java/helpers/FakeHelper.java
+++ src/test/java/helpers/FakeHelper.java
@@ -1,10 +1,7 @@
 package helpers;
 
 import com.github.javafaker.Faker;
-
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 public class FakeHelper {
     Faker faker = new Faker(new Locale("en-GB"));
@@ -15,23 +12,23 @@ public class FakeHelper {
             gender = faker.options().option("Female", "Male", "Other"),
             phoneNumber = faker.phoneNumber().subscriberNumber(10),
             yearOfBirth = String.valueOf(faker.number().numberBetween(1900, 2020)),
-            monthOfBirth = faker.options().option("January","August", "May", "February", "March"),
+            monthOfBirth = faker.options().option("January", "August", "May", "February", "March"),
             dayOfBirth = String.format("%02d", faker.number().numberBetween(1, 29)),
             subject = faker.options().option("Math", "Economics", "English", "History"),
             hobbies = faker.options().option("Sports", "Music", "Reading"),
             picture = "image3.png",
             currentAddress = faker.address().fullAddress(),
             state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan"),
-            city =  faker.options().option(CityHelper.stateCitiesMap.get(state));
+            city = findCityByState(state);
 
-    public static class CityHelper {
-        private static final Map<String, String[]> stateCitiesMap = new HashMap<>();
+    public String findCityByState(String state) {
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> "";
+        };
 
-        static {
-            stateCitiesMap.put("NCR", new String[]{"Delhi", "Gurgaon", "Noida"});
-            stateCitiesMap.put("Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"});
-            stateCitiesMap.put("Haryana", new String[]{"Karnal", "Panipat"});
-            stateCitiesMap.put("Rajasthan", new String[]{"Jaipur", "Jaiselmer"});
-        }
     }
 }
